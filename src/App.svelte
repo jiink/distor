b951f497ffbf089bff6b6c646654a819899a9428
Yves <vyves246@gmail.com>
2022-09-23 08:32:38 -0700

    Small Changes
    Removed SVG from the site.
    
    Changed color of button that takes picture
    
    When take picture button is pressed it downloads the photo to your computer

--- src/App.svelte
+++ src/App.svelte
@@ -4,30 +4,8 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://acmcsuf.com" target="_blank"> 
-      <img src={devLogo} class="logo dev" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Svelte Website Template. Will become... distor!</h1>
 
   <div class="card">
     <Counter />
   </div>
 </main>
-
-<style>
-  .logo {
-    height: em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-
-  .logo.dev:hover {
-    filter: drop-shadow(0 0 2em #1E6CFF);
-  }
-</style>
