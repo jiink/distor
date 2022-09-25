f7a73f838abdf00ab53fdd65ff84dcfd7740b5c5
Yves <83028938+HallowsYves@users.noreply.github.com>
2022-09-25 08:11:39 -0700

    Slider Bar Added
    Created slide bar
    
    Removed card counter from template
    
    Created some base code for some customization of the slider bar.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,7 @@
 <script>
-  import devLogo from './assets/webcam.svg'
-  import Counter from './lib/Counter.svelte'
+
 </script>
 
 <main>
 
-  <div class="card">
-    <Counter />
-  </div>
 </main>
